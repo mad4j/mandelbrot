ac37379cb43c2147f005a71f39375f55a0e2cb24
Daniele Olmisani <daniele.olmisani@gmail.com>
2025-03-16 15:34:28 +0100

    Update field_map.rs

--- src/field_map.rs
+++ src/field_map.rs
@@ -1,37 +1,46 @@
-use super::complex::Z;
+use num::Complex;
 
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub struct FieldMap {
-    pub upper_left: Z,
-    //pub lower_right: Z,
     pub re_resolution: usize,
     pub im_resolution: usize,
-    pub re_delta: f64,
-    pub im_delta: f64,
+    pub precomputed_re: Vec<f64>,
+    pub precomputed_im: Vec<f64>,
 }
 
 impl FieldMap {
-    pub fn new(upper_left: Z, lower_right: Z, re_resolution: usize, im_resolution: usize) -> Self {
+    pub fn new(
+        upper_left: Complex<f64>,
+        lower_right: Complex<f64>,
+        re_resolution: usize,
+        im_resolution: usize,
+    ) -> Self {
+        let re_delta = (lower_right.re - upper_left.re) / re_resolution as f64;
+        let im_delta = (upper_left.im - lower_right.im) / im_resolution as f64;
+
+        let precomputed_re: Vec<f64> = (0..re_resolution)
+            .map(|x| upper_left.re + x as f64 * re_delta)
+            .collect();
+
+        let precomputed_im: Vec<f64> = (0..im_resolution)
+            .map(|y| upper_left.im - y as f64 * im_delta)
+            .collect();
+
         FieldMap {
-            upper_left,
-            //lower_right,
             re_resolution,
             im_resolution,
-            re_delta: (lower_right.re() - upper_left.re()) / re_resolution as f64,
-            im_delta: (upper_left.im() - lower_right.im()) / im_resolution as f64,
+            precomputed_re,
+            precomputed_im,
         }
     }
 
-    pub fn get_point(self, index: usize) -> Z {
+    pub fn get_point(&self, index: usize) -> Complex<f64> {
         let (x, y) = (index % self.re_resolution, index / self.re_resolution);
 
-        Z::new(
-            self.upper_left.re() + x as f64 * self.re_delta,
-            self.upper_left.im() - y as f64 * self.im_delta,
-        )
+        Complex::new(self.precomputed_re[x], self.precomputed_im[y])
     }
 
-    pub fn get_limit(self) -> usize {
+    pub fn get_limit(&self) -> usize {
         self.re_resolution * self.im_resolution
     }
 }
