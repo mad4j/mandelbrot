3bfdc3b3454cddea50a04147f01650d13f17e39e
Daniele Olmisani <daniele.olmisani@gmail.com>
2025-03-22 09:59:43 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod field_map;
 
 use clap::{Parser, Subcommand};
 use compute_mono::MandelbrotMono;
+use ocl::{core::DeviceInfo, Device, Platform};
 
 use std::time::Instant;
 
@@ -46,8 +47,42 @@ pub struct MandelbrotComputationResult {
     elapsed_time: std::time::Duration,
 }
 
+pub fn dump_opencl_info() -> Result<()> {
+    let platforms = Platform::list();
+    
+    for (p_idx, platform) in platforms.iter().enumerate() {
+        println!("Platform {}: {:?}", p_idx, platform.name()?);
+        println!("  Vendor: {:?}", platform.vendor()?);
+        println!("  Version: {}", platform.version()?);
+        println!("  Profile: {:?}", platform.profile()?);
+        println!("  Extensions: {:?}", platform.extensions()?);
+        
+        let devices = Device::list_all(platform)?;
+        
+        for (d_idx, device) in devices.iter().enumerate() {
+            println!("  Device {}: {:?}", d_idx, device.name()?);
+            println!("    Vendor: {:?}", device.vendor()?);
+            println!("    Version: {}", device.version()?);
+            println!("    Type: {:?}", device.info(DeviceInfo::Type)?);
+            println!("    Profile: {}", device.info(DeviceInfo::Profile)?);
+            println!("    Max Compute Units: {:?}", device.info(DeviceInfo::MaxComputeUnits)?);
+            println!("    Max Work Group Size: {:?}", device.info(DeviceInfo::MaxWorkGroupSize)?);
+            println!("    Max Work Item Dimensions: {:?}", device.info(DeviceInfo::MaxWorkItemDimensions)?);
+            println!("    Max Work Item Sizes: {:?}", device.info(DeviceInfo::MaxWorkItemSizes)?);
+            println!("    Max Clock Frequency: {:?}", device.info(DeviceInfo::MaxClockFrequency)?);
+            println!("    Max Memory Allocation Size: {:?}", device.info(DeviceInfo::MaxMemAllocSize)?);
+            println!("    Global Memory Size: {:?}", device.info(DeviceInfo::GlobalMemSize)?);
+            println!("    Local Memory Size: {:?}", device.info(DeviceInfo::LocalMemSize)?);
+        }
+    }
+    
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let cli = Cli::parse();
+
+    dump_opencl_info()?;
 
     // Dimensioni dell'immagine
     let width = 4000;
@@ -62,14 +97,15 @@ fn main() -> Result<()> {
 
     let result = match &cli.command {
         Commands::Ocl {} => {
-            MandelbrotOcl::compute(width, height, max_iters, upper_left, lower_right)?
-        }
+                        MandelbrotOcl::compute(width, height, max_iters, upper_left, lower_right)?
+            }
         Commands::Rayon {} => {
-            MandelbrotRayon::compute(width, height, max_iters, upper_left, lower_right)?
-        }
+                MandelbrotRayon::compute(width, height, max_iters, upper_left, lower_right)?
+            }
         Commands::Mono {} => {
-            MandelbrotMono::compute(width, height, max_iters, upper_left, lower_right)?
-        }
+                MandelbrotMono::compute(width, height, max_iters, upper_left, lower_right)?
+            }
+
     };
 
     let elapsed_time = start_time.elapsed();
